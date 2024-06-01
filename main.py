6378cb3b74e60cdb98a65abf84cc87b7a0e82bed
sk0493 <166520562+sk0493@users.noreply.github.com>
2024-06-01 05:26:39 +0100

    small comments

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ sq_size = height / dimension
 images = {} # global dictionary of images
 
 def load_images():
-    # Using pygame to load in the chess images
+    '''
+    Using pygame to load in the chess images.
+    Here indexing on the dictionary will make it easier to draw the individual chess pieces on the board later.
+    '''
+
     pieces = ["bR", "bN", "bB", "bQ", "bK", "bN", "bP", "wN", "wB", "wQ", "wK", "wN", "wR", "wP"]
     for piece in pieces:
         images[piece] = pg.transform.scale(pg.image.load("images/" + piece + ".png"), (sq_size,sq_size))
@@ -20,11 +24,9 @@ def load_images():
 def draw_game_state(screen, gs):
     '''
     Responsible for all the graphics whtin a current game state. 
-    Draws the checkers on the board and pieces.
+    Draws the checkers on the board and the pieces.
     '''
     draw_board(screen, gs.board)
-    #draw_pieces(screen, gs.board) # draw chess pieces on top of board
-
 
 def draw_board(screen, board):
     colours = [pg.Color("white"), pg.Color("brown")]
@@ -35,15 +37,6 @@ def draw_board(screen, board):
             piece = board[row][column]
             if piece != "--": # not empty cell
                 screen.blit(images[piece], pg.Rect(column*sq_size, row*sq_size, sq_size,sq_size))
-
-
-# def draw_pieces(screen, board):
-#     for row in range(dimension):
-#         for column in range(dimension):
-#             piece = board[row][column]
-#             if piece != "--": # not empty cell
-#                 screen.blit(images[piece], pg.Rect(column*sq_size, row*sq_size, sq_size,sq_size))
-
 
 def main():
     '''
@@ -60,11 +53,11 @@ def main():
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 running = False
+                break
         
         draw_game_state(screen, gs)
         clock.tick(15)
         pg.display.flip()
-
 
 
 if __name__ == "__main__":
